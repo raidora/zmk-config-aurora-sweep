fc3ae9484cb790ea82e5558d1f65c283b7b3284a
Raido Oras <oras.raido@gmail.com>
2024-04-09 04:02:34 +0300

    WIP keymap

---
+++ config/est_definitions.h
@@ -0,0 +1,76 @@
+//Special characters for estonian keyboard layout
+#define ODIA                        SEMICOLON 
+#define OTIL                        RIGHT_BRACKET
+#define ADIA                        APOSTROPHE
+#define UDIA                        LEFT_BRACKET
+#define EE_LEFT_ANGLE_BRACKET       &kp NUBS
+#define EE_RIGHT_ANGLE_BRACKET      &kp LS(NUBS)
+#define EE_LEFT_PARENTHESIS         &kp LS(N8)
+#define EE_RIGHT_PARENTHESIS        &kp LS(N9)
+#define EE_LEFT_CURLY_BRACKET       &kp RA(N7)
+#define EE_RIGHT_CURLY_BRACKET      &kp RA(N0)
+#define EE_LEFT_SQUARE_BRACKET      &kp RA(N8)
+#define EE_RIGHT_SQUARE_BRACKET     &kp RA(N9)
+
+#define EE_DOLLAR                   &kp RA(N4)
+#define EE_AMPERSAND                &kp LS(N6)
+#define EE_EQUALS                   &kp LS(N0)
+#define EE_UNDERSCORE               &kp LS(SLASH)
+#define EE_DOUBLE_QUOTE             &kp LS(N2)
+#define EE_SINGLE_QUOTE             &kp BACKSLASH
+#define EE_PIPE                     &kp RA(NUBS)
+#define EE_MINUS                    &kp SLASH
+#define EE_PLUS                     &kp MINUS
+#define EE_CIRCUMFLEX               &kp RA(APOS)
+#define EE_BACKTICK                 &kp LS(EQUAL)
+#define EE_HASHTAG                  &kp LS(N3)
+#define EE_QUESTION_MARK            &kp LS(MINUS)
+#define EE_SEMICOLON               &kp LS(COMMA)
+#define EE_COLON                    &kp LS(PERIOD)
+#define EE_EXCLAMATION_MARK         &kp LS(N1)
+#define EE_ASTERISK                 &kp LS(BACKSLASH)
+#define EE_BACKSLASH                &kp RA(MINUS)
+#define EE_FORWARDSLASH             &kp LS(N7)
+#define EE_COMMERCIAL_AT            &kp RA(N2)
+#define EE_PERCENT                  &kp LS(N5)
+#define EE_TILDE                    &kp LS(GRAVE)
+#define EE_CARON                    &kp GRAVE
+#define EE_EURO                     &kp RA(N5)
+
+// Brackets
+#define EE_LABK                     EE_LEFT_ANGLE_BRACKET
+#define EE_RABK                     EE_RIGHT_ANGLE_BRACKET
+
+#define EE_LCBR                     EE_LEFT_CURLY_BRACKET
+#define EE_RCBR                     EE_RIGHT_CURLY_BRACKET
+
+#define EE_LBRC                     EE_LEFT_SQUARE_BRACKET
+#define EE_RBRC                     EE_RIGHT_SQUARE_BRACKET
+
+#define EE_LPRN                     EE_LEFT_PARENTHESIS
+#define EE_RPRN                     EE_RIGHT_PARENTHESIS
+
+// Symbols
+#define EE_SCLN                     EE_SEMICOLON
+#define EE_COLN                     EE_COLON
+#define EE_USCR                     EE_UNDERSCORE
+#define EE_QSTN                     EE_QUESTION_MARK
+#define EE_BKTK                     EE_BACKTICK
+#define EE_HSTG                     EE_HASHTAG
+#define EE_EQLS                     EE_EQUALS
+#define EE_AMPR                     EE_AMPERSAND
+#define EE_MINS                     EE_MINUS
+#define EE_DOLR                     EE_DOLLAR
+#define EE_EXCL                     EE_EXCLAMATION_MARK
+#define EE_ASTR                     EE_ASTERISK
+
+#define EE_SLSH                     EE_FORWARDSLASH
+#define EE_BSLS                     EE_BACKSLASH
+
+#define EE_DQUO                     EE_DOUBLE_QUOTE
+#define EE_QUOT                     EE_SINGLE_QUOTE
+#define EE_ATSI                     EE_COMMERCIAL_AT
+#define EE_PERC                     EE_PERCENT
+#define EE_TLDE                     EE_TILDE
+#define EE_CARN                     EE_CARON
+#define EE_CIRC                     EE_CIRCUMFLEX
